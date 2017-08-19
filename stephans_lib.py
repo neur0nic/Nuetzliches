d5e99abf1f5beeb9d510bd12cce94d511519a207
Stephan Mertens <steph_cs@gmx.de>
2017-08-19 09:32:58 +0200

    Added a little routine to stop programs when enter key is hit.

--- stephans_lib.py
+++ stephans_lib.py
@@ -7,10 +7,21 @@ from time import sleep
 from termcolor import colored
 
 
-def progressBar(current, total):
+def progressbar(current, total):
     i = round((current / total) * 100, 1)
     strdone = int(round(i, 0)) * '█'
     strundone = (100 - int(round(i, 0))) * '█'
     totstr = "|" + colored(strdone, 'green') + colored(strundone, 'red') + "| " + str(i) + "%"
     print(totstr, end='\r')
     if current == total: print()
+
+''' Thread to abort program.
+    Can be used to stop progams with the 'enter'-key.
+'''
+from threading import Thread
+
+
+def waittoabort():
+    list = []
+    abort = Thread(target=stopp, args=(list,))
+    abort.start()
